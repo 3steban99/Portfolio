1689f755354915b74bd97af63d47c6a475034861
3steban99 <barrionuevo99@outlook.com>
2023-06-28 19:49:01 -0300

    cambios en el nav y en el main.js

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 const menu = document.querySelector(".nav-responsive");
+const navBar = document.querySelector(".nav-bar");
 
-menu.onclick = function(){
-    const navBar = document.querySelector(".nav-bar");
+menu.onclick = function () {
     navBar.classList.toggle("active")
 }
+
+function seleccionar(){
+    navBar.classList.remove("active")
+}
+
+document.addEventListener("touchmove", function () {
+    navBar.classList.remove("active")
+});
