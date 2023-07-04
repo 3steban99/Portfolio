bb33cf661c88eadfd2b1960b094a3dcdee91edf2
3steban99 <barrionuevo99@outlook.com>
2023-07-04 14:27:23 -0300

    cambios y agregacion de idioma

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const menu = document.querySelector(".nav-responsive");
 const navBar = document.querySelector(".nav-bar");
 
+
 menu.onclick = function () {
     navBar.classList.toggle("active")
 }
@@ -13,3 +14,24 @@ document.addEventListener("touchmove", function () {
     navBar.classList.remove("active")
 });
 
+
+//Languages
+
+const flagsElement = document.getElementById("flags");
+const textsToChange = document.querySelectorAll("[data-section]")
+
+const changeLanguage = async language => {
+    const requestJson = await fetch(`./languages/${language}.json`);
+    const texts = await requestJson.json();
+
+    for (const textToChange of textsToChange) {
+        const section = textToChange.dataset.section;
+        const value = textToChange.dataset.value;
+
+        textToChange.innerHTML= texts[section][value];
+    }
+}
+
+flagsElement.addEventListener("click", (e) => {
+    changeLanguage(e.target.parentElement.dataset.language);
+})
